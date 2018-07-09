1b03780a642c6db7fafaaf107490ee5b48d62716
Harry Frank <harry.frank@1901group.com>
2018-07-09 10:08:46 -0400

    Started doing things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LogsComponent } from './logs/logs.component';
+import { LogFormComponent } from './log-form/log-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LogsComponent,
+    LogFormComponent
   ],
   imports: [
     BrowserModule
